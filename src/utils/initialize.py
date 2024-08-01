9b0c6d0e235dd6fd6a5e0c6f036c521d58b583e8
n4okins <63445867+n4okins@users.noreply.github.com>
2024-08-02 03:32:22 +0900

    feat: Refactor initialize.py and path.py
    Refactor the `initializer` function in `initialize.py` to improve code readability and maintainability. Also, refactor the `parent_search` and `get_current_dir` functions in `path.py` to enhance their clarity and efficiency.
    
    This commit addresses the recent user commit "add get_current_dir" and the repository commits "chore: Update .gitignore and dependencies", "feat: Add colored logging functionality".

--- src/utils/initialize.py
+++ src/utils/initialize.py
@@ -8,28 +8,48 @@ from typing import Optional
 from dotenv import load_dotenv
 
 from .clogging import getColoredLogger
-from .path import parent_search
+from .path import get_current_dir, parent_search
 
 
 def initializer(
-    root: Path | str,
+    globals_: dict[str, object],
     logger: Optional[logging.Logger] = getColoredLogger(__name__),
     *,
     dotenv_filename: str = ".env",
-) -> None:
+) -> Path:
+    """Initialize the environment
+    Load the .env file and set the HUGGINGFACE_HUB_CACHE environment variable.
+
+    Args:
+        globals_ (dict[str, object]): The globals() dictionary.
+        logger (Optional[logging.Logger], optional): The logger. Defaults to getColoredLogger(__name__).
+        dotenv_filename (str, optional): The .env filename. Defaults to ".env".
+
+    Returns:
+        project_root_directory (Path): The detected project root directory.
+        equal to __file__ (Python environment) or the current working directory (Jupyter environment).
+    """
     logger.info("Initializing...")
-    root = Path(root)
-    DOTENV_PATH = parent_search(root, dotenv_filename)
-    logger.info(f"{DOTENV_PATH=}")
-    load_dotenv(dotenv_path=root.parent / dotenv_filename)
+    project_root_directory = Path(get_current_dir(globals_))
+    DOTENV_PATH = parent_search(
+        project_root_directory, dotenv_filename, enable_return_none=True
+    )
+    if DOTENV_PATH is None:
+        logger.info("Could not detect .env file")
+    else:
+        logger.info(f"{DOTENV_PATH=}")
+        load_dotenv(dotenv_path=project_root_directory.parent / dotenv_filename)
 
     HUGGINGFACE_HUB_CACHE = os.environ.get("HUGGINGFACE_HUB_CACHE", None)
     logger.info(f"{HUGGINGFACE_HUB_CACHE=}")
+
     CONTROL_SERVER_PREFIX = os.environ.get("CONTROL_SERVER_PREFIX", None)
-    logger.info(f"{CONTROL_SERVER_PREFIX=}")
     if CONTROL_SERVER_PREFIX is not None:
+        logger.info(f"{CONTROL_SERVER_PREFIX=}")
         HOSTNAME = os.uname()[1]
         logger.info(f"{HOSTNAME=}")
         assert not HOSTNAME.startswith(
             CONTROL_SERVER_PREFIX
         ), f"{HOSTNAME=}, {CONTROL_SERVER_PREFIX=}"
+
+    return project_root_directory
--- src/utils/path.py
+++ src/utils/path.py
@@ -1,25 +1,44 @@
 from pathlib import Path
-from typing import Callable
 
 
-def parent_search(path: Path, name: str) -> Path:
+def parent_search(
+    path: Path, name: str, enable_return_none: bool = True
+) -> Path | None:
     """
     Search for a directory with the given name in the parent directories of the given path.
+    Args:
+        path (Path): The starting directory.
+        name (str): The directory name to search for.
+        enable_return_none (bool, optional): Whether to raise an error if the directory is not found. Defaults to True.
+
+    Returns:
+        Path: The directory with the given name.
+        or None if the directory is not found and can_raise_error is False.
     """
     while True:
         if (path / name).exists():
             return path / name
-        if path == path.parent:
+        if path == path.parent and not enable_return_none:
             raise FileNotFoundError(
                 f"Could not find {name} in the parent directories of {path}"
             )
         elif path == Path("/"):
-            raise FileNotFoundError(
-                f"Could not find {name} in the parent directories of {path}"
-            )
+            if not enable_return_none:
+                raise FileNotFoundError(
+                    f"Could not find {name} in the parent directories of {path}"
+                )
+            else:
+                return None
         path = path.parent
 
 
-def get_current_dir(_globals: Callable[[], dict]) -> Path:
-    _file_ = _globals().get("__file__", None)
+def get_current_dir(_globals_dict: dict) -> Path:
+    """Get the current directory of the Python script or Jupyter notebook.
+    If the script is running in a Jupyter notebook, return the current working directory.
+    Args:
+        _globals_dict (dict): The globals() dictionary.
+    Returns:
+        Path object: The current directory.
+    """
+    _file_ = _globals_dict.get("__file__", None)
     return Path.cwd() if _file_ is None else Path(_file_).parent
