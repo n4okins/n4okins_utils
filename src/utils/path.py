9b45f9dcf35f9227fba4591031c1695d65448c50
n4okins <63445867+n4okins@users.noreply.github.com>
2024-08-01 18:49:17 +0900

    add get_current_dir

--- src/utils/path.py
+++ src/utils/path.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Callable
 
 
 def parent_search(path: Path, name: str) -> Path:
@@ -17,3 +18,8 @@ def parent_search(path: Path, name: str) -> Path:
                 f"Could not find {name} in the parent directories of {path}"
             )
         path = path.parent
+
+
+def get_current_dir(_globals: Callable[[], dict]) -> Path:
+    _file_ = _globals().get("__file__", None)
+    return Path.cwd() if _file_ is None else Path(_file_).parent
